92d6e8104a64951df2010ad80e84965340bca3f9
Micah Jo <micahjo@google.com>
2024-02-07 16:14:43 -0800

    Add recipe for InAndOutDirectoryOperationRequest.toTransform API
    Bug: n/a
    Test: this is a test
    Change-Id: I431610b0f4887ac582400fbc3ddd1bf545ff0e2c

---
+++ recipes/transformDirectory/build-logic/plugins/src/main/kotlin/CheckAssetsTask.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import java.lang.RuntimeException
+
+abstract class CheckAssetsTask : DefaultTask() {
+
+    // In order for the task to be up-to-date when the inputs have not changed,
+    // the task must declare an output, even if it's not used. Tasks with no
+    // output are always run regardless of whether the inputs changed
+    @get:OutputDirectory
+    abstract val output: DirectoryProperty
+
+    @get:InputDirectory
+    abstract val assetsDirectory: DirectoryProperty
+
+    @TaskAction
+    fun taskAction() {
+        val transformedFileContents = File(assetsDirectory.get().asFile, "FileToTransform.txt").readText()
+        if (transformedFileContents != "transformed content") {
+            throw RuntimeException("Transformed file does not have expected contents.")
+        }
+    }
+}
